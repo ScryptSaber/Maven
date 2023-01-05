7075a14442752637a2d48b91c5853cddfa6a3a1c
erdem <aerdemkamis@gmail.com>
2023-01-05 15:25:20 +0300

    Maven Projects added

--- src/test/java/rahulshettyacademy/AutocompleteDropDown.java
+++ src/test/java/rahulshettyacademy/AutocompleteDropDown.java
@@ -25,6 +25,7 @@ public class AutocompleteDropDown {
             if (opt.getText().equalsIgnoreCase("India")) {
                 opt.click();
                 break;
+                
             }
         }
     }
