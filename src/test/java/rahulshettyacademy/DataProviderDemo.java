6c63f5d75622438a77163ad43f0f7d1031ce4ffe
erdem <aerdemkamis@gmail.com>
2023-01-12 15:49:50 +0300

    Maven Project added

---
+++ src/test/java/rahulshettyacademy/DataProviderDemo.java
@@ -0,0 +1,56 @@
+package rahulshettyacademy;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class DataProviderDemo {
+    DataFormatter formatter = new DataFormatter();
+
+    @Test(dataProvider = "driveTestExcel")
+    public void testCaseData(String greeting, String communication, String id) {
+        System.out.println(greeting + "-" + communication + "-" + id);
+    }
+
+    @DataProvider(name = "driveTest")
+    public Object[][] getData() {
+        Object[][] data = {{"hello", "text", 1}, {"bye", "message", 143}, {"solo", "call", 453}};
+        return data;
+    }
+
+
+    @DataProvider(name = "driveTestExcel")
+    public Object[][] getExcelData() throws IOException {
+
+
+        FileInputStream fis = new FileInputStream("C:\\Users\\ASUS\\Desktop\\dataDemo.xlsx");
+        XSSFWorkbook wb = new XSSFWorkbook(fis);
+        XSSFSheet sheet = wb.getSheetAt(0);
+
+        int rowNumber = sheet.getPhysicalNumberOfRows();
+        XSSFRow row = sheet.getRow(0);
+        int colNumber = row.getLastCellNum();
+
+        Object[][] data = new Object[rowNumber - 1][colNumber];
+
+        for (int i = 0; i < data.length; i++) {
+            row = sheet.getRow(i + 1);
+
+            for (int j = 0; j < data[0].length; j++) {
+
+                XSSFCell cell = row.getCell(j);
+                data[i][j] = formatter.formatCellValue(cell);
+            }
+        }
+        return data;
+    }
+
+
+}
